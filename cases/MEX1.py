85728cb6204a9b74f7781a88c0e28bd549230635
elemermelada <iiasanmi9@gmail.com>
2023-07-20 14:27:49 +0200

    improved MEX1

--- cases/MEX1.py
+++ cases/MEX1.py
@@ -10,11 +10,13 @@ from tudatpy.kernel.numerical_simulation import estimation, estimation_setup
 from tudatpy.kernel.numerical_simulation.estimation_setup import observation
 
 from util.environment_setup import add_radiation_pressure, add_tw_stations
+from util.estimation_postprocessing import retrieve_best_iteration_state_history
+from util.observation_postprocessing import (
+)
 from util.observation_setup import (
     add_noise,
     add_observation_simulators,
     add_simple_doppler_observation_settings,
-    add_simple_range_observation_settings,
     add_viability_check,
     create_ow_links,
 )
@@ -25,12 +27,18 @@ from util.estimation_setup import create_parameters_to_estimate
 
 from util.graphs import (
     init_trajectory_graph,
+    plot_ephemeris,
     plot_mars,
     plot_trajectory_from_spice,
 )
 from util.point_distributions import fibonacci_sphere
 
-from init.MEX10TW import bodies, simulation_start_epoch, simulation_end_epoch, tw_number
+from init.MEX10TWSHORT import (
+    bodies,
+    simulation_start_epoch,
+    simulation_end_epoch,
+    tw_number,
+)
 
 # Add radiation pressure to environment
 add_radiation_pressure(bodies, environment_setup)
@@ -56,52 +64,53 @@ light_time_correction_settings = (
 observation_times = np.arange(simulation_start_epoch, simulation_end_epoch, 60.0)
 
 # Add doppler "sensors"
+TYPE = observation.one_way_instantaneous_doppler_type
 observation_settings_list = add_simple_doppler_observation_settings(
     observation,
     links,
     light_time_correction_settings=light_time_correction_settings,
 )
 observation_simulation_settings = add_observation_simulators(
-    observation, observation_times, links, observation.one_way_range_type
+    observation, observation_times, links, TYPE
 )
 add_noise(
     observation,
     1.0e-3,
-    observation.one_way_instantaneous_doppler_type,
+    TYPE,
     observation_simulation_settings,
 )
 add_viability_check(
     observation,
-    observation.one_way_instantaneous_doppler_type,
+    TYPE,
     np.deg2rad(15),
     observation_simulation_settings,
     links,
 )
 
-# Add range "sensors"
-observation_settings_list = add_simple_range_observation_settings(
-    observation,
-    links,
-    light_time_correction_settings=light_time_correction_settings,
-    observation_settings_list=observation_settings_list,
-)
-observation_simulation_settings = add_observation_simulators(
-    observation,
-    observation_times,
-    links,
-    observation.one_way_instantaneous_doppler_type,
-    observation_simulation_settings=observation_simulation_settings,
-)
-add_noise(
-    observation, 1.0, observation.one_way_range_type, observation_simulation_settings
-)
-add_viability_check(
-    observation,
-    observation.one_way_range_type,
-    np.deg2rad(15),
-    observation_simulation_settings,
-    links,
-)
+# # Add range "sensors"
+# observation_settings_list = add_simple_range_observation_settings(
+#     observation,
+#     links,
+#     light_time_correction_settings=light_time_correction_settings,
+#     observation_settings_list=observation_settings_list,
+# )
+# observation_simulation_settings = add_observation_simulators(
+#     observation,
+#     observation_times,
+#     links,
+#     observation.one_way_instantaneous_doppler_type,
+#     observation_simulation_settings=observation_simulation_settings,
+# )
+# add_noise(
+#     observation, 1.0, observation.one_way_range_type, observation_simulation_settings
+# )
+# add_viability_check(
+#     observation,
+#     observation.one_way_range_type,
+#     np.deg2rad(15),
+#     observation_simulation_settings,
+#     links,
+# )
 
 # Create observations
 simulated_observations = perform_observations(
@@ -111,6 +120,7 @@ simulated_observations = perform_observations(
     bodies,
     observation_simulation_settings,
 )
+
 
 # Estimate
 propagator_settings_estimation = basic_propagator(
@@ -133,3 +143,9 @@ estimator = create_estimator(
     propagator_settings_estimation,
 )
 estimation_output = estimate(estimation, estimator, simulated_observations)
+
+state_history = retrieve_best_iteration_state_history(estimation_output, clean=True)
+ax = plot_ephemeris(ax, state_history * 1e-3, color="k", linestyle="--")
+ax.legend()
+fig.tight_layout()
+fig.savefig("out/estimate.png")
