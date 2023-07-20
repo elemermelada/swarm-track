98a59214b885c698d350017fea731d9d5e862523
elemermelada <iiasanmi9@gmail.com>
2023-07-20 19:17:04 +0000

    fixed 3d

--- util/graphs.py
+++ util/graphs.py
@@ -5,14 +5,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_ephemeris(ax: Axes, ephemeris, axis=[1, 2], color="b", linestyle="-"):
+def plot_ephemeris(
+    ax: Axes, ephemeris, axis=[1, 2], threeD=False, color="b", linestyle="-"
+):
+    if threeD:
+        ax.plot(
+            ephemeris[:, 0],
+            ephemeris[:, 1],
+            ephemeris[:, 2],
+            color=color,
+            linestyle=linestyle,
+        )
+        return ax
     ax.plot(
         ephemeris[:, axis[0]], ephemeris[:, axis[1]], color=color, linestyle=linestyle
     )
     return ax
 
 
-def scatter_ephemeris(ax: Axes, ephemeris, axis=[1, 2], color="b", linestyle="-"):
+def scatter_ephemeris(
+    ax: Axes, ephemeris, axis=[1, 2], threeD=False, color="b", linestyle="-"
+):
+    if threeD:
+        ax.scatter(
+            ephemeris[:, 0],
+            ephemeris[:, 1],
+            ephemeris[:, 2],
+            color=color,
+            linestyle=linestyle,
+        )
+        return ax
     ax.scatter(
         ephemeris[:, axis[0]], ephemeris[:, axis[1]], color=color, linestyle=linestyle
     )
@@ -25,6 +47,7 @@ def plot_trajectory_from_spice(
     simulation_start_epoch,
     simulation_end_epoch,
     axis=[1, 2],
+    threeD=False,
     color="b",
     linestyle="-",
 ):
@@ -41,18 +64,26 @@ def plot_trajectory_from_spice(
             )
         )
     ephemeris = np.array(ephemeris) * 1e-3
-    return plot_ephemeris(ax, ephemeris, axis, color, linestyle)
+    return plot_ephemeris(ax, ephemeris, axis, threeD, color, linestyle)
 
 
-def plot_mars(ax: Axes):
+def plot_mars(ax: Axes, threeD=False):
     radius_mars = spice.get_average_radius("Mars")
+    if threeD:
+        ax.scatter(0, 0, 0, color="firebrick")
+        return ax
     ax.add_patch(
         plt.Circle((0, 0), radius_mars * 1e-3, color="firebrick", label="Mars")
     )
     return ax
 
 
-def init_trajectory_graph():
+def init_trajectory_graph(threeD):
+    if threeD:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d")
+        return (ax, fig)
+
     fig, ax = plt.subplots(1, 1, figsize=(9, 6))
     ax.axis("equal")
     ax.set_xlabel(r"$\Delta y$ [km]")
